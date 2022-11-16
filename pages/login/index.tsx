a37fc544de59ba958ac80086bde085058164cf87
DucilioManjate <liomanjate@gmail.com>
2022-11-15 22:46:24 -0300

    login

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,11 +1,25 @@
 import { use, useState } from "react"
+import { useAuth } from "../contexts/auth";
 
 export default function Login() {
-    const [ form, setForm] = useState({username:'', password:''})
-    
+    const [form, setForm] = useState({ username: '', password: '' })
+    const { signed, Login } = useAuth();
+    console.log(signed);
+
+    async function handleLogin() {
+        await Login({
+            email: 'ducilio.manjate@test.com',
+            password: '1234',
+        });
+    }
+
     return (
         <>
-            LOgin
+            <div>
+                <button onClick={handleLogin}>Login</button>
+            </div>
         </>
     )
 }
+
+
