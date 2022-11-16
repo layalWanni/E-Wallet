a23d40b389e012f5aaf403edc93bba9e26d3577b
DucilioManjate <liomanjate@gmail.com>
2022-11-15 22:33:18 -0300

    api service

---
+++ pages/login/index.tsx
@@ -0,0 +1,11 @@
+import { use, useState } from "react"
+
+export default function Login() {
+    const [ form, setForm] = useState({username:'', password:''})
+    
+    return (
+        <>
+            LOgin
+        </>
+    )
+}
